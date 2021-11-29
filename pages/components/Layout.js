ecd58744e80f020a82f7fb9abe25373cb6bb75f6
keysi.fernandes <keysi.fernandes@hypeflame.com.br>
2021-11-29 18:37:59 -0300

    fix: fix my social media urls

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -3,8 +3,8 @@ export default function Layout({ children }) {
     <div className="px-64 py-4 min-h-screen bg-gray-800 flex flex-col justify-between">
       {children}
       <footer className="flex flex-col items-center justify-center w-full">
-        <h3 className=" text-center text-3xl text-white">Redes sociais</h3>
-        <div className="bg-gray-700 py-3 rounded-lg text-white w-full text-center justify-between">
+        <h3 className="mb-4 text-center text-3xl text-white">Redes sociais</h3>
+        <div className="bg-gray-700 py-3 rounded-lg text-white w-full text-center justify-between mb-2">
           <button
             onClick={() => window.open("https://linkedin.com/in/keysijones")}
             className="bg-blue-400 mx-2 px-2 rounded text-xl"
@@ -12,13 +12,13 @@ export default function Layout({ children }) {
             LinkedIn
           </button>
           <button
-            onClick={() => window.open("https://linkedin.com/in/keysijones")}
+            onClick={() => window.open("https://twitter.com/keysi_jones")}
             className="bg-gray-800 mx-2 px-2 rounded text-xl"
           >
             Twitter
           </button>
           <button
-            onClick={() => window.open("https://linkedin.com/in/keysijones")}
+            onClick={() => window.open("https://keysijones.vercel.app/")}
             className="bg-green-600 mx-2 px-2 rounded text-xl"
           >
             Portfólio
