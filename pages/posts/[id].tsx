3092f3a5c9b1b26f4e3503d1c6dd179f78c72ca0
KeysiJones <keysijones02@gmail.com>
2022-04-02 19:32:09 -0300

    Feat: uses typescript on post detail page

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,17 +2,29 @@ import { useEffect } from "react";
 import { AboutAvatar, HomePageLink, PostHeader, PostBody } from '../../components'
 const BASE_URL = process.env.REACT_APP_BLOG_API;
 
+type Context = {
+  params: {
+    id: number;
+  };
+}
+
+type PostDetailProps = {
+  title: string,
+  subtitle: string,
+  body: string
+};
+
 export async function getStaticPaths() {
   const apiResponse = await fetch(`${BASE_URL}/posts/`);
   const postList = await apiResponse.json();
-  const paths = postList.map((post) => ({
+  const paths = postList.map((post: { id: { toString: () => any; }; }) => ({
     params: { id: post.id.toString() },
   }));
 
   return { paths, fallback: false };
 }
 
-export async function getStaticProps(context) {
+export async function getStaticProps(context: Context) {
   const { id } = context.params;
   const apiResponse = await fetch(`${BASE_URL}/posts/${id}`);
 
@@ -21,7 +33,7 @@ export async function getStaticProps(context) {
   return { props: { title, subtitle, body } };
 }
 
-export default function postDetail({ title, subtitle, body }) {
+export default function postDetail({ title, subtitle, body }: PostDetailProps) {
   const avatarImage = "/images/cropped-keysi.jpg";
   const avatarName = "Keysi Jones";
   const avatarUrl = "https://keysijones.vercel.app";
@@ -32,9 +44,9 @@ export default function postDetail({ title, subtitle, body }) {
 
   return (
     <div className="mb-8">
-      <PostHeader title={title} subtitle={subtitle}/>
-      <HomePageLink/>
-      <PostBody body={body}/>
+      <PostHeader title={title} subtitle={subtitle} />
+      <HomePageLink />
+      <PostBody body={body} />
       <AboutAvatar
         avatarUrl={avatarUrl}
         avatarName={avatarName}
