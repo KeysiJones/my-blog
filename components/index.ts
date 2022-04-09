966125f476e7a19076d20bb18e2116fb68b5b484
keysijones <keysijones02@gmail.com>
2022-04-09 20:11:07 -0300

    feat: clear

--- components/index.ts
+++ components/index.ts
@@ -4,7 +4,7 @@ export * from './avatar';
 export * from './footer';
 export * from './homePageLink';
 export * from './layout';
-export * from './loader';
+export * from './loadingSpinner';
 export * from './postHeader';
 export * from './postBody';
 export * from './socialMediaIcons';
